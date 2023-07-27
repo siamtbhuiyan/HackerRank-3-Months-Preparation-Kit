defe4cf6f931b16310bce73dab41d9e0e701055c
Siam T. Bhuiyan <siamtbhuiyan@gmail.com>
2023-07-27 16:29:41 +0600

    Solved Problem 6: Divisible Sum Pairs

---
+++ week1/6_divisibleSumPairs.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'divisibleSumPairs' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER k
+#  3. INTEGER_ARRAY ar
+#
+
+def divisibleSumPairs(n, k, ar):
+    count = 0
+    for index1, i in enumerate(ar):
+        for index2, j in enumerate(ar):
+            if index1 < index2 and (i + j) % k == 0:
+                count += 1
+    return count
+
+if __name__ == '__main__':
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    k = int(first_multiple_input[1])
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = divisibleSumPairs(n, k, ar)
+
+    print(result)
